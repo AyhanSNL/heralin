e44e5990da92682e087b13a64cab47ba1eb6708a
s.ayhansenel@gmail.com <s.ayhansenel@gmail.com>
2023-11-05 19:12:36 +0300

    "deneme"

--- home-page.php
+++ home-page.php
@@ -14,6 +14,7 @@ if ($bakim['durum'] == '1' ) {
 <head>
     <base href="<?php echo"$ayar[site_url]"?>">
     <meta http-equiv="Content-Type" content="text/html;charset=UTF-8" />
+    <meta  content="deneme" />
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="<?php echo $ayar['site_url'] ?>images/<?php echo $ayar['site_favicon']; ?>">
